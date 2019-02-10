7193ac5c0203de49574396113b167cace6d53c88
Dante van der Heijden <dante@scholica.com>
2019-02-10 16:56:33 +0100

    Added the R files

--- Week 1/Week_1.R
+++ Week 1/Week_1.R
@@ -5,9 +5,19 @@ setspotify()
 
 # Load required playlist
 
-pharrell <- get_playlist_audio_features('dantesean', '37LPgjZWSjykXkrhHYzPcR')
+pharrell_unfiltered <- get_playlist_audio_features('dantesean', '37LPgjZWSjykXkrhHYzPcR')
 pop2000 <- get_playlist_audio_features('11123270324', '2n8WeZO6kAku8y4FNpDEka')
+
+# Clean the Pharrell playlist for duplicates due to different album versions
+
+pharrell <- pharrell_unfiltered[!duplicated(pharrell_unfiltered$track_name), ]
 
 # Summarise key patterns
 
+pharrell %>% summarise(M = mean(danceability), SD = sd(danceability))
+pharrell %>% summarise(M = mean(energy), SD = sd(energy))
 pharrell %>% summarise(M = mean(valence), SD = sd(valence))
+
+pop2000 %>% summarise(M = mean(danceability), SD = sd(danceability))
+pop2000 %>% summarise(M = mean(energy), SD = sd(energy))
+pop2000 %>% summarise(M = mean(valence), SD = sd(valence))
